aedc499d217f49c91a3241b78730c48d1e005ff1
Ezabulbary <ezabul.bary@gmail.com>
2022-07-06 15:33:20 +0600

    all component pages added

---
+++ src/component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <p id='home' className=''>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, animi?</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='footer'>© 2021, Softcent EU, A European IT company</p>
+        </div>
+    );
+};
+
+export default Footer;
