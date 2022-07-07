ae699ecec62a976d57b7525ad1b1ae30bec17de2
Ezabulbary <ezabul.bary@gmail.com>
2022-07-07 19:05:17 +0600

    product section added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Customer from '../Pages/Customer/Customer';
 import Hero from '../Pages/Hero/Hero';
+import Products from '../Pages/Products/Products';
 import Work from '../Pages/Work/Work';
 import PageTitle from '../shared/PageTitle/PageTitle';
 import './Home.css'
@@ -8,10 +9,11 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-            <PageTitle title='Home'></PageTitle>
+            <PageTitle title='Home' />
             <Hero/>
             <Customer/>
             <Work/>
+            <Products/>
         </div>
     );
 };
--- src/component/Pages/Products/Products.js
+++ src/component/Pages/Products/Products.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import PageTitle from '../../shared/PageTitle/PageTitle';
 import './Products.css';
+import PageTitle from '../../shared/PageTitle/PageTitle';
+import Underline from '../../../Images/underline.png';
+import product1 from '../../../Images/product/phone-ui1.png'
+import product2 from '../../../Images/product/phone-ui.png'
+import { Link } from 'react-router-dom';
 
 const Products = () => {
     return (
-        <div>
-            <PageTitle title='Premium Products'></PageTitle>
-            <h1>This is Products section</h1>
-        </div>
+        <section id='product-id'>
+            <div>
+                <PageTitle title='Premium Products'></PageTitle>
+                <div className='product-title-container'>
+                    <p className='product-page-title'>Our Products</p>
+                    <img className='product-underline' src={Underline} alt='underlineImage' />
+                    <div className='product-sub-title-container'>
+                        <p className='product-page-sub-title'>We believe that great design should not be out of reach, so our services are less than half the price of a full-time designer.</p>
+                        <Link to='/products'><button className='product-btn1'>See More</button></Link>
+                    </div>
+                </div>
+
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='product-details'>
+                            <p className='product-title'>SwipeXYZ Product</p>
+                            <p className='product-sub-title'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <button className='product-btn'>View product</button>
+                        </div>
+                        <div>
+                            <img className='product-image' src={product1} alt="productImage" />
+                        </div>
+                    </div>
+                    <div className='product1'>
+                        <div>
+                            <img className='product-image' src={product2} alt="productImage" />
+                        </div>
+                        <div className='product-details'>
+                            <p className='product-title'>Cashback Product</p>
+                            <p className='product-sub-title'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <button className='product-btn'>View product</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
